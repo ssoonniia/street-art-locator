e989e3e4a97c6878ae66d09b545a117da0ecbb16
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-05-15 15:21:51 -0700

    Update creatUser action -- now sets user when response received from API

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,6 +1,15 @@
 export function createUser(name, age){
   return(dispatch)=>{
     const body = JSON.stringify({name: name, age: age})
+    console.log(body)
     dispatch({type: 'CREATE_USER'});
+    fetch('/users', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: body
+    })
+    .then(response => response.json())
+    .then(data => dispatch({ type: 'SET_USER', data: data}))
   }
 }
+
