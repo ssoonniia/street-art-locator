8482a9c6a8f38e005c2287898e4620bcfbb7b742
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-05-15 15:20:09 -0700

    Add styling for app and background image

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/Home';
+import './App.css';
 
 const App = props => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={HomePage}/>
-      </Switch>
-    </BrowserRouter> 
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={HomePage}/>
+        </Switch>
+      </BrowserRouter> 
+    </div>
   );
 }
 export default App;
