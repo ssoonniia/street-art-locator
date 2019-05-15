14d2a5bc2b270f008c5c6320f675f84f329406e0
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-05-15 02:43:17 -0700

    Add user form

---
+++ client/src/pages/Form.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+
+class Form extends Component {
+  state = {
+			name: '',
+			age: '',
+	}
+
+	handleNameChange = ( event ) => {
+		this.setState({ name: event.target.value })
+	}
+
+	handleAgeChange = ( event ) => {
+		this.setState({ age: event.target.value })
+		console.log(this.state.age)
+	}
+
+	handleOnSubmit = ( event ) => {
+		alert('I was submitted')
+		event.preventDefault()
+	}
+
+	render(){
+		return (
+			<form onSubmit={ this.handleOnSubmit }>
+				<label>
+					Name:
+					<input type="text" value={ this.state.name } onChange={ this.handleNameChange }/>
+				</label>
+				<label>
+					Age:
+					<input type="number" value={ this.state.age } onChange={ this.handleAgeChange }/>
+				</label>
+				<input type="submit" value="Submit"/>
+			</form>
+		);
+	}
+}
+export default Form;
