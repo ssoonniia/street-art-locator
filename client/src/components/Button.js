57e19089f3a77ccf34ae699e839929fb68b409aa
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-05-16 22:07:10 -0700

    Add link to button
    and fix spacing in home page

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom'
 
 const HomeButton = props => {
 
@@ -15,7 +16,7 @@ const HomeButton = props => {
 
   return(
     <div style={wrapper}>
-      <Button style={buttonStyle}variant="contained" color="primary" >
+      <Button component={Link} to="/map" style={buttonStyle}variant="contained" color="primary" >
         Get Started
       </Button>
     </div>
