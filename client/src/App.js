ca6b9354b09faf136b0e736b3dc317100f1359f5
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-05-28 20:36:40 -0700

    Add comment to routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/Home';
 import MapGL from './components/Map';
+import Form from './pages/Form';
+import User from './pages/User';
+import Comment from './pages/CommentsContainer'
 import './App.css';
 import ButtonAppBar from './components/ButtonAppBar'
 
@@ -13,6 +16,9 @@ const App = props => {
         <Switch>
           <Route exact path="/" component={HomePage}/>
           <Route exact path="/map" component={MapGL}/>
+          <Route exact path="/form" component={Form}/>
+          <Route exact path="/user" component={User}/>
+          <Route exact path="/comments" component={Comment}/>
         </Switch>
       </BrowserRouter> 
     </div>
