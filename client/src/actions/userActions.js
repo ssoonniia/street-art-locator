95382d5c2195f9e7802f1a4fdfbac829aad7d2d2
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-05-28 23:08:13 -0700

    Add create comment action

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,7 +1,6 @@
 export function createUser(name, age){
   return(dispatch)=>{
     const body = JSON.stringify({name: name, age: age})
-    console.log(body)
     dispatch({type: 'CREATE_USER'});
     fetch('/users', {
       method: 'POST',
@@ -11,5 +10,20 @@ export function createUser(name, age){
     .then(response => response.json())
     .then(data => dispatch({ type: 'SET_USER', payload: data}))
   }
+}
+
+export function createComment(author, comment){
+  return(dispatch)=>{
+    const body = JSON.stringify({author: author, comment: comment})
+  dispatch({type: 'CREATE_COMMENT'});
+  fetch('/comments',{
+    method: 'POST', 
+    headers: {'Content-Type': 'application/json'},
+    body: body 
+  })
+  .then(response => response.json())
+  .then(data => dispatch({type: 'ADD_COMMENT', payload: data}))
+  }
+  
 }
 
