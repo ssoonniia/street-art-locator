2df0b0d81e1c15d732574a364f54d78e3344d662
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-05-28 23:03:00 -0700

    Add comments to sate and create comment and add comment reducers

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,12 +1,18 @@
 export default function rootReducer( state = {
   isFetchingUser: false,
+  isCreatingComment: false,
   user: [],
+  comments: [],
 }, action ){
   switch (action.type){
     case 'CREATE_USER':
       return { ...state, isFetchingUser: true  }  
     case 'SET_USER':
-      return { isFetchingUser: false, user: action.payload  }  
+      return { ...state, isFetchingUser: false, user: action.payload }  
+    case 'CREATE_COMMENT' :
+      return {...state, isCreatingComment: true}
+    case 'ADD_COMMENT':
+      return {...state, isCreatingComment: false ,comments: state.comments.concat(action.payload)} 
     default: 
     return state
   }
