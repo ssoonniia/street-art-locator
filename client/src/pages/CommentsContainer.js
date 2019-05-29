bf95088eca99f8b223a0a1b0c571bf9fe7890c50
Sonia Duarte Nicas <sduarte22@gmail.com>
2019-05-28 23:02:16 -0700

    Add comment form and render comments

--- client/src/pages/CommentsContainer.js
+++ client/src/pages/CommentsContainer.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import Comments from '../components/Comments/Comments'; 
+import {createComment} from '../actions/userActions'
 
 class CommentsContainer extends Component {
 
@@ -13,6 +15,46 @@ class CommentsContainer extends Component {
     }
 
     handleCommentOnChange = ( event ) => {
-        this.setState({ comment: event.targe.value })
+        this.setState({ comment: event.target.value })
     }
+    handleOnSubmit = ( event ) => {
+        this.props.createComment(this.state.author, this.state.comment)
+        event.preventDefault()
+        this.setState({
+          author: '',
+          comment: ''  
+        })
+    }
+
+    render(){
+        return(
+        <div>
+            <form onSubmit={this.handleOnSubmit}>
+                <label>By:</label>
+                <input
+                    type="text"
+                    name="author"
+                    onChange={this.handleAuthorOnChange} 
+                    value={this.state.author} 
+                />
+                <label>Comment </label>
+                <textarea
+                    type="textarea"
+                    name="comment"
+                    onChange={this.handleCommentOnChange}
+                    value={this.state.comment}
+                />
+                <input type="submit" value="Submit" />
+            </form>
+            <Comments comments={this.props.comments} />
+        </div>
+        )
+    }
+
 }
+
+function mapStateToProps(state){
+    return { comments: state.comments }
+}
+  
+export default connect(mapStateToProps, {createComment})(CommentsContainer);
